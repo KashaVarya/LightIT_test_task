58f50e6eebd59b3da5588e58caba6f1652f06ecd
Varya <varyax@yandex.ru>
2018-03-25 13:55:51 +0300

    fixed programm header

--- main.cpp
+++ main.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 int main()
 {
-    cout << "============================================\n"
-         << "Program for convertation of Roman numerals into Arabic and Arabic into Roman numerals\n"
-         << "============================================\n";
+    cout << "========================================================================================\n"
+         << "Program for the translation of Roman numerals into Arabic and Arabic into Roman numerals\n"
+         << "========================================================================================\n";
 
 
     string num;
